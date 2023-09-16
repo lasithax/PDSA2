3025ed2f7b7c9d858fc57221c7cea9e833f5c0b6
Lasitha Udayanga <ludayanga@ariasystems.com>
2023-09-16 22:40:48 +0530

    Initial commit

---
+++ src/main/java/com/example/pdsa/PdsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pdsa/PdsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
