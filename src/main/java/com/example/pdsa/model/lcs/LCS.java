22c42247f25055e62b6657ea4cdf4bca138e7538
Yuneth Wijenayake <yunethcwij@gmail.com>
2023-09-30 00:47:33 +0530

    Completed LCS

--- src/main/java/com/example/pdsa/model/lcs/LCS.java
+++ src/main/java/com/example/pdsa/model/lcs/LCS.java
@@ -4,18 +4,19 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.Map;
 
 @Document(collection = "lcs")
 public class LCS {
     private ObjectId _id;
     private String name;
-    private List<String> strings;
+    private Map<String, String> strings;
     private String response;
 
     public LCS() {
     }
 
-    public LCS(ObjectId _id, String name, List<String> strings, String response) {
+    public LCS(ObjectId _id, String name, Map<String, String> strings, String response) {
         this._id = _id;
         this.name = name;
         this.strings = strings;
@@ -38,11 +39,11 @@ public class LCS {
         this.name = name;
     }
 
-    public List<String> getStrings() {
+    public Map<String, String> getStrings() {
         return strings;
     }
 
-    public void setStrings(List<String> strings) {
+    public void setStrings(Map<String, String> strings) {
         this.strings = strings;
     }
 
--- src/main/java/com/example/pdsa/service/LCSService.java
+++ src/main/java/com/example/pdsa/service/LCSService.java
@@ -1,4 +1,39 @@
 package com.example.pdsa.service;
 
+import com.example.pdsa.logic.LCSGame;
+import com.example.pdsa.model.lcs.LCS;
+import com.example.pdsa.repository.LCSRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class LCSService {
+
+    @Autowired
+    private LCSRepository lcsRepo;
+
+    private LCSGame lcsGame;
+    private String username;
+    public Map<String, String> start(String username){
+        this.username = username;
+        lcsGame = new LCSGame();
+        return lcsGame.getStrings();
+    }
+
+    public Map<String, String> checkAnswer(String answer){
+        boolean isCorrect = lcsGame.checkAnswer(answer);
+
+        if (isCorrect){
+            addNewResponse(username, lcsGame.getStrings(), answer);
+        }
+
+        return isCorrect ? Map.of("result", "Correct!") : Map.of("result", "Incorrect!");
+    }
+
+    public void addNewResponse(String username, Map<String, String> strings, String response){
+        lcsRepo.save(new LCS(new ObjectId(), username, strings, response));
+    }
 }
